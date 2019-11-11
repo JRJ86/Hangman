f1b6b249794a3365c3aeac68d96525fe20cd2db1
jacob <jacobriis86@hotmail.com>
2019-11-11 09:51:49 +0100

    making TODO´s

--- app/src/main/java/dk/riis/jacob/hangman/TODOS.java
+++ app/src/main/java/dk/riis/jacob/hangman/TODOS.java
@@ -3,4 +3,6 @@ package dk.riis.jacob.hangman;
 public class TODOS {
   //TODO - nothing at the moment. Its all done
     //TODO - change app icon
+    //TODO - make own keyboard
+    //TODO - make a list to choose something from
 }
